43b90c7cab16465e15ce3ed11670f1d4f9ee7e86
Hao <haohu.2011@hotmail.com>
2020-03-04 01:11:26 +0800

    simple random value generator

--- src/main.go
+++ src/main.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
+	"math"
+	"math/rand"
+	"time"
+)
+
+type ByteSize float64
+const (
+	_ = iota
+	KB ByteSize = 1<<(10*iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
 )
 
 func main() {
-	var goos string = runtime.GOOS
-	fmt.Printf("The OS is: %s\n", goos)
-	path := os.Getenv("PATH")
-	fmt.Printf("Path is %s\n", path)
+	Random()
+    n := 20.1
+    _ = IntFromFloat64(n)
+}
+
+func IntFromFloat64(x float64) int {
+	if math.MinInt32 <= x && x <= math.MaxInt32 {
+		whole, fraction := math.Modf(x)
+		if fraction >= 0.5 {
+			whole++
+		}
+		return int(whole)
+	}
+	panic(fmt.Sprintf("%g is out of int32 range", x))
+}
+
+func Random() {
+	for i := 0; i < 10; i++ {
+		a := rand.Int()
+		fmt.Printf("%d / ", a)
+	}
+
+	for i := 0; i < 5; i++ {
+		r := rand.Intn(8)
+		fmt.Printf("%d / ", r)
+	}
+
+	fmt.Println()
+	timens := int64(time.Now().Nanosecond())
+	rand.Seed(timens)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%2.2f / ", 100*rand.Float32())
+	}
 }
