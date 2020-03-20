b245ce5b0b33e025e496c69eb16103654c392849
Hao <haohu.2011@hotmail.com>
2020-03-20 23:45:36 +0800

    an employee management system

---
+++ src/empMgrSystem.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type employee struct {
+	id int
+	name string
+}
+
+type ems struct {
+	employee map[int]employee
+}
+
+func (e ems) showEmployee() {
+	for _, v := range e.employee {
+		fmt.Printf("ID: %d, Name: %s\n", v.id, v.name)
+	}
+}
+
+func (e ems) addEmployee() {
+	var (
+		empID int
+		empName string
+	)
+	fmt.Print("new employee ID is: ")
+	fmt.Scanln(&empID)
+	fmt.Print("new employee Name is: ")
+	fmt.Scanln(&empName)
+	newEmp := employee{
+		id: empID,
+		name: empName,
+	}
+	e.employee[newEmp.id] = newEmp
+}
+
+func (e ems) editEmployee() {
+	var(
+		empID int
+		empName string
+	)
+	fmt.Print("which employee you want to edit? please provide the ID: ")
+	fmt.Scanln(&empID)
+	empObj, err := e.employee[empID]
+	if !err {
+		fmt.Println("there is no such employee")
+		return
+	}
+	fmt.Printf("the employee you want to edit is - ID: %d, Name: %s\n", empObj.id, empObj.name)
+	fmt.Print("please enter the new name: ")
+	fmt.Scanln(&empName)
+	empObj.name = empName
+	e.employee[empID] = empObj
+	fmt.Println("done")
+}
+
+func (e ems) deleteEmployee() {
+	var empID int
+	fmt.Print("which employee you want to delete? please provide the ID: ")
+	fmt.Scanln(&empID)
+	_, err := e.employee[empID]
+	if !err {
+		fmt.Println("there is no such employee")
+		return
+	}
+	delete(e.employee, empID)
+	fmt.Println("done")
+}
--- src/main.go
+++ src/main.go
@@ -2,21 +2,48 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os"
 )
 
-func main() {
-	a := makeAddSuffix(".jpeg")
-	b := makeAddSuffix(".png")
-	fmt.Println(a("pic1"))
-	fmt.Println(b("pic2"))
+var (
+	empMgrSys ems
+)
+
+func showMenu() {
+	fmt.Println("welcome to Employee Management System")
+	fmt.Println(`
+		1. show all employees
+		2. add employee
+		3. edit employee
+		4. delete employee
+		5. exit
+	`)
 }
 
-func makeAddSuffix(suffix string) func(fileName string) string {
-	return func(fileName string) string {
-		if !strings.HasSuffix(fileName, suffix) {
-			return fileName + suffix
-		}
-		return fileName
+func main() {
+	empMgrSys = ems{
+		employee: make(map[int]employee, 100),
 	}
+	for {
+		showMenu()
+		fmt.Print("please make a choice: ")
+		var choice int
+		fmt.Scanln(&choice)
+		fmt.Printf("your choice is: %d\n", choice)
+		switch choice {
+		case 1:
+			empMgrSys.showEmployee()
+		case 2:
+			empMgrSys.addEmployee()
+		case 3:
+			empMgrSys.editEmployee()
+		case 4:
+			empMgrSys.deleteEmployee()
+		case 5:
+			os.Exit(1)
+		default:
+			fmt.Println("invalid choice, try again!")
+		}
+	}
+
 }
